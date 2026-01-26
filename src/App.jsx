ca0dff7cff0c2bb56a66305baa4a617f6804f427
Atharva Jadhav <jadhavatharva4203@gmail.com>
2026-01-26 15:40:54 -0800

    React Day2: controlled input and bounded counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,64 @@
+// Input Mirror (Controlled output)
+// import { useState } from "react";
+
+// export default function App() {
+//   const [text, setText] = useState("")
+
+//   function typing(e) {
+//     setText(e.target.value)
+//   }
+
+//   return (
+//     <div>
+//       <h1>Input Mirror</h1>
+//       <input type="text" value={text} onChange={typing}/>
+//       <p>You typed : {text}</p>
+//     </div>
+//   )
+//   }
+
+// Counter with Rule
 import { useState } from "react";
 
 export default function App() {
-  const [count, setCount] = useState(0);
+  const[count, setCount] = useState(0)
+  const[isDisabledInc, setIsDisabledInc] = useState(false);
+  const[isDisabledDec, setIsDisabledDec] = useState(true);
 
   function increment() {
-    setCount(count + 1);
+    if (count < 10) {
+      const next = count + 1;
+      setCount(next);
+      setIsDisabledDec(false)
+      setIsDisabledInc(next >= 10)
+    }
+    else {
+      setIsDisabledDec(false)
+      setIsDisabledInc(true)
+    }
   }
 
   function decrement() {
-    setCount(count - 1);
+    if (count > 0) {
+      const next = count - 1;
+      setCount(next);
+      setIsDisabledDec(next <= 0)
+      setIsDisabledInc(false)
+    }
+    else {
+      setIsDisabledDec(true)
+      setIsDisabledInc(false)
+    }
   }
 
-  function reset() {
-    setCount(0);
-  }
+  
 
   return (
     <div>
-      <h1>Counter</h1>
-      <h2>{count}</h2>
-
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
-      <button onClick={reset}>Reset</button>
+      <h1>Counter with Rules</h1>
+      <p>{count}</p>
+      <button type="button" disabled={isDisabledInc} onClick={increment} >Increment</button>
+      <button type="button" disabled={isDisabledDec} onClick={decrement}>Decrement</button>
     </div>
-  );
+  )
 }
